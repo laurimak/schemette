0216eaf450dfb10353b2654049b0efbbc6139b61
Lauri Mäkinen <lauri.makinen@solita.fi>
2014-05-05 15:34:26 +0300

    Cleanup code.

--- src/main/java/schemette/environment/DefaultEnvironment.java
+++ src/main/java/schemette/environment/DefaultEnvironment.java
@@ -1,42 +1,38 @@
 package schemette.environment;
 
 import com.google.common.collect.ImmutableMap;
-import schemette.expressions.*;
+import schemette.expressions.Expression;
+import schemette.expressions.NumberExpression;
+import schemette.expressions.SymbolExpression;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.BinaryOperator;
-import java.util.function.Function;
 
-import static schemette.expressions.BooleanExpression.*;
+import static schemette.expressions.BooleanExpression.bool;
+import static schemette.expressions.ProcedureExpression.procedure;
 import static schemette.expressions.SymbolExpression.symbol;
 
 public class DefaultEnvironment {
     private static final ImmutableMap<SymbolExpression, Expression> PRIMITIVES = ImmutableMap.<SymbolExpression, Expression>builder()
             .put(symbol("+"),
-                    function((args) -> longFunction(args, (a, b) -> a + b)))
+                    procedure(args -> longFunction(args, (a, b) -> a + b)))
             .put(symbol("-"),
-                    function((args) -> longFunction(args, (a, b) -> a - b)))
+                    procedure(args -> longFunction(args, (a, b) -> a - b)))
             .put(symbol("/"),
-                    function((args) -> longFunction(args, (a, b) -> a / b)))
+                    procedure(args -> longFunction(args, (a, b) -> a / b)))
             .put(symbol("*"),
-                    function((args) -> longFunction(args, (a, b) -> a * b)))
+                    procedure(args -> longFunction(args, (a, b) -> a * b)))
             .put(symbol("="),
-                    function((args) -> bool(args.stream()
-                            .allMatch((e) -> e.equals(args.get(0))))))
+                    procedure(args -> bool(args.stream().allMatch(e -> e.equals(args.get(0))))))
             .put(symbol("#t"),
                     bool(true))
             .put(symbol("#f"),
                     bool(false))
-
                             .build();
 
     public static Environment newInstance() {
         return new Environment(new HashMap<>(PRIMITIVES));
-    }
-
-    private static ProcedureExpression function(Function<List<Expression>, Expression> f) {
-        return ProcedureExpression.procedure(f);
     }
 
     private static NumberExpression longFunction(List<Expression> args, BinaryOperator<Long> accumulator) {
